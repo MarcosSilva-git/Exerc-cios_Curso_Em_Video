de2451bfd6b252fca25d5abd21625d6334ddef4e
MarcosSilva-git <marcosjoseoliveira.mj@gmail.com>
2020-04-05 19:20:18 -0300

    Exercícios 110 e 111

--- Exercícios_110/Exercício_108.py
+++
@@ -1,7 +0,0 @@
-from Exercícios_109 import moeda
-
-num = float(input('Digite um preço: R$'))
-print(f'O dobro de {moeda.moeda(num)} é {moeda.dobro(num, "U$")}')
-print(f'A metade de {moeda.moeda(num)} é {moeda.metade(num, True)}')
-print(f'Adicionando 10% fica {moeda.aumentar(num, 10, True)}')
-print(f'Retirando 13% fica {moeda.diminuindo(num, 13, True)}')
---
+++ Exercícios_110/Exercício_110.py
@@ -0,0 +1,5 @@
+from Exercícios_110 import moeda
+
+print('='*30)
+num = float(input('Digite um preço: R$'))
+moeda.resumo(num)
